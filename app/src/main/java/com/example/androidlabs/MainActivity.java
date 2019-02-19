6257fbddf280662dd0820b9de8ce499703e67a23
Adavid <davi0519>
2019-02-18 22:15:38 -0500

    Initial commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.androidlabs;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText email;
     @Override
-    protected void onCreate(Bundle onTest) {
-        super.onCreate(onTest);
-        setContentView(R.layout.activity_main_relative);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences sharedPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
+        String email = sharedPrefs.getString("myPrefs", "");
+
     }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        final EditText email = findViewById(R.id.editText4);
+        String emailText = email.getText().toString();
+        intent.putExtra("email_Text", emailText);
+
+        startActivity(intent);
+    }
+
+            @Override
+    public void onPause() {
+        super.onPause();
+                SharedPreferences sharedPrefs =
+                        getSharedPreferences("myPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor  editor = sharedPrefs.edit();
+                editor.putString("email", email.getText().toString());
+                editor.apply();
+    }
+
 }
